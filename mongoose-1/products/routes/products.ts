db1bd22140ce19ef0186db9ec1f448304141708b
sanskrati-1234 <sanskratiagrawal306@gmail.com>
2024-04-19 19:59:50 +0530

    connect to mongoose

--- mongoose-1/products/routes/products.ts
+++ mongoose-1/products/routes/products.ts
@@ -3,6 +3,22 @@ import { ProductData, ProductModel } from "../schema/Product";
 
 const productRouter: Router = express.Router();
 
+productRouter.get("/", async (req: Request, res: Response) => {
+  try {
+    const data = await ProductModel.find({ isActive: true });
+    res.status(200).send({
+      data: data,
+      meta: { message: "Product added successfully" },
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).send({
+      data: {},
+      meta: { message: "Unable to take your request. Please try later" },
+    });
+  }
+});
+
 productRouter.post("/", async (req: Request, res: Response) => {
   try {
     const { name, description }: ProductData = req.body;
